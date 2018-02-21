583b1e88150a2c3c70424965e245acca91a56bcd
Daniel Nyga <nyga@cs.uni-bremen.de>
2018-02-21 18:16:07 +0100

    fixes in practell, make storage in DB optional and nont-default

--- python2/practell.py
+++ python2/practell.py
@@ -22,13 +22,14 @@ def main():
     usage = 'PRAC Tell'
 
     parser = argparse.ArgumentParser(description=usage)
-    parser.add_argument('--howto', '-H', type=str, dest='howto', help='Title of the howto, e.g. "Make pancakes"')
-    parser.add_argument('--steps', '-s', type=str, nargs='+', dest='steps', default=False, action='store_true', help='A list of instruction steps in natural language. If set, his option must be the last in the list of options followed by the list of instructions.')
+    parser.add_argument('--howto', '-H', type=str, help='Title of the howto, e.g. "Make pancakes"')
+    parser.add_argument('--steps', '-s', nargs='+', type=str, dest='steps', help='A list of instruction steps in natural language. If set, his option must be the last in the list of options followed by the list of instructions.')
     parser.add_argument('--batch', '-b', dest='batch', default=False, action='store_true', help='Import a list of howtos in batch processing whose filenames are given the respective howto title, e.g. "Make a pancake." The file content must then be given by the single instruction steps, one by line.')
     parser.add_argument('--recursive', '-r', dest='recursive', default=False,  help='Apply the import of instructions recursively to subdirectories.')
     parser.add_argument("--verbose", "-v", dest="verbose", default=1, type=int, action="store", help="Set verbosity level {0..3}. Default is 1.")
     parser.add_argument('--quiet', '-q', dest='quiet', action='store_true', default=False, help='Do not print any status messages.')
     parser.add_argument('--multicore', '-m', dest='multicore', action='store_true', default=False, help='Perform information extraction in multicore modus')
+    parser.add_argument('--save', action='store_true', default=False, help='Store the howto in the PRAC database permanently.')
 
     args = parser.parse_args()
     opts_ = vars(args)
@@ -86,7 +87,7 @@ def import_howto(args):
         prac = PRAC()
         prac.verbose = verbose
         for howto, steps in list(howto_steps.items()):
-            prac.tell(howto=howto, steps=steps)
+            prac.tell(howto=howto, steps=steps, save=args.save)
     except KeyboardInterrupt: return
 
 if __name__ == '__main__':
