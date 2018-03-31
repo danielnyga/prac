fa345f025e2c03cb9be37ed93d889c8ff1a80e25
Daniel Nyga <nyga@cs.uni-bremen.de>
2018-04-01 00:19:31 +0200

    added deterministic grounding

--- python2/prac/pracmodules/grounding/src/grounding.py
+++ python2/prac/pracmodules/grounding/src/grounding.py
@@ -26,11 +26,50 @@ def newsymbol(mln, db, domain):
 
 class Grounding(PRACModule):
 
-    def __call__(self, node, objdb, constraints=None):
+    def __call__(self, infer, worldmodel, constraints=None):
+        return self.deterministic_grounding(infer, worldmodel, constraints)
+
+    def deterministic_grounding(self, infer, worldmodel, constraints):
+        newframes = []
+        frames = [n.frame for n in infer.steps()]
+        for frame in frames:
+            skipframe = False
+            for role, obj in frame.actionroles.items():
+                frame.actionroles[role].type = list(set([o.type for o in worldmodel.getall(obj)]))
+                # out(frame.toplan(), frame.actionroles[role].type)
+                if not frame.actionroles[role].type:
+                    if not frame.mandatory:
+                        out('skipping frame', frame.toplan(), 'because it is not mandatory')
+                        skipframe = True
+                        break
+                    else:
+                        raise Exception(str(frame.toplan()) + ' is not executable.')
+            if skipframe:
+                continue
+            queue = [frame]
+            while queue:
+                f = queue.pop(0)
+                modified = False
+                for role, obj in f.actionroles.items():
+                    if type(obj.type) is list:
+                        f_ = None
+                        if len(obj.type) == 1:
+                            obj.type = obj.type[0]
+                        else:
+                            f_ = f.copy()
+                            f_.actionroles[role].type = obj.type.pop()
+                            modified = True
+                    if modified:
+                        queue.append(f)
+                        queue.append(f_)
+                        break
+                if not modified:
+                    newframes.append(f)
+        return frames
+
+    def probabilistic_grounding(self, node, worldmodel, constraints):
         logger.info('{} {}'.format(self.name, node))
-
         constraints = ifnone(constraints, {})
-
         self.roles = self.prac.module('senses_and_roles')
         self.wnmod = self.prac.module('wn_senses')
 
@@ -71,15 +110,15 @@ class Grounding(PRACModule):
         # out(concept2role)
         # out(role2concept)
         db = PRACDatabase(self.prac)
-        db << 'action_core(actioncore, %s)' % actioncore.name
+        # db << 'action_core(actioncore, %s)' % actioncore.name
         for obj in objects:
             for id_, concept in obj.items():
                 db << 'has_sense(%s, %s)' % (id_, concept)
                 db << 'is_a(%s,%s)' % (concept, concept)
-                if concept in concept2role:
+                if concept in concept2role: # here come the exactly matching instances
                     db << '%s(%s,%s)' % (concept2role[concept], id_, actioncore.name)
         project.queryconf['cw'] = True
-        project.queryconf['queries'] = [r for r in actioncore.roles]  #  if r not in role2concept]
+        project.queryconf['queries'] = [r for r in actioncore.roles if r != 'action_verb']  #  if r not in role2concept]
         # copy over the role constraints
         # TODO: this hs to go into the ROS module
         for role, concepts in constraints.items():
@@ -92,9 +131,9 @@ class Grounding(PRACModule):
         simil = self.wnmod.add_sims(db, tmpmln)
         # we need senses and similarities as well as original evidence
         db = db.union(simil)
-        db.write()
+        db.write(bars=False, color=False)
         out(project.queryconf['queries'])
-        infer = MLNQuery(config=project.queryconf, mln=mln, db=db, verbose=0).run()
+        infer = MLNQuery(config=project.queryconf, mln=mln, db=db, verbose=1).run()
         resultdb = PRACDatabase(self.prac, db=infer.resultdb)
         resultdb.write()
         resultdb = db.union(resultdb, mln=self.prac.mln)
@@ -103,3 +142,4 @@ class Grounding(PRACModule):
         # ==============================================================
         for role, concept in resultdb.roles(actioncore.name):
             yield role, concept
+
